12ad44a4ca7c227844312a82abcf25cbb8b138fb
unknown <ishworineupane135@gmail.com>
2021-06-12 18:40:09 +0545

    init

---
+++ src/pages/AddTodo.tsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function AddTodo() {
+    const [data, setData] = useState([]);
+    const [message, setMessage] = useState("" as any);
+    const [input, setInput] = useState("" as any);
+
+    const fetchdata = async () => {
+        let response= await axios.get(" http://localhost:40000/posts");
+        setData(response.data);
+    }
+    useEffect(() => {
+        fetchdata();
+   
+    }, [])
+    const handleInput = (event:any) => {
+        event.persist();
+        setMessage(event.target.value);
+         setInput("" as string);
+    }
+    const handleSubmit =async (event:any) => {
+        event.persist();
+        let response = await axios.post("http://localhost:40000/posts", { message: message });
+        fetchdata();
+       
+    }
+
+    return (
+        <div>
+        <div className="message__head">
+            {data.map((message:any)=>
+            (
+                <div className="message" id={message.id}>
+                <h1 className="message__title">{message.message}</h1>
+                    <h1 className="message__id">{message.id}</h1>
+                    </div>
+            ))}
+                </div>
+            <input type="text" onChange={handleInput} name="message" value={message} id="message" />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default AddTodo
